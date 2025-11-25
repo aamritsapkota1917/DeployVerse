9dafe69ce591c37c9e1cb3a018e5fe3995676288
aamritsapkota1917 <amritsapkota7350@gmail.com>
2025-11-25 11:16:37 +0545

    feat:  integrate loki and promtail

--- Backend/Utils/logger.js
+++ Backend/Utils/logger.js
@@ -2,22 +2,11 @@ import winston from "winston";
 
 const consoleFormat = winston.format.combine(winston.format.colorize(), winston.format.simple());
 
-const fileFormat = winston.format.combine(
-  winston.format.timestamp(),
-  winston.format.printf(({ timestamp, level, message }) => {
-    return `$ ${level.toUpperCase()}: ${message}`;
-  })
-);
-
 export const info_logger = winston.createLogger({
   level: "info",
   transports: [
     new winston.transports.Console({
       format: consoleFormat,
-    }),
-    new winston.transports.File({
-      filename: "info.log",
-      format: fileFormat,
     }),
   ],
 });
@@ -27,10 +16,6 @@ export const error_logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: consoleFormat,
-    }),
-    new winston.transports.File({
-      filename: "error.log",
-      format: fileFormat,
     }),
   ],
 });
